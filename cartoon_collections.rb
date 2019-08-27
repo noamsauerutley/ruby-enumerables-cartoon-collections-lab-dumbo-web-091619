c11c6f67f5b5f714ed163187162c66557dafc5fa
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-27 03:28:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+   counter = 0
+   while counter < array.length
+     puts "#{i + 1}. #{array[i]}"
+     counter +=1
 end
 
 def summon_captain_planet# code an argument here
