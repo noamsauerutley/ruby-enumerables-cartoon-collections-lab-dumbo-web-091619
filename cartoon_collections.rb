439cfee6c678fe497bed118f954b9e92cbc92f69
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-27 03:49:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,8 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect(|el|
-
-  )
+  cheese_types each do |cheese|
+    array.detect {|el| el == cheese}
+  end
+  nil
 end
